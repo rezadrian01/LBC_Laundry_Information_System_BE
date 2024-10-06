b6128b360a9579e26a42dd93478d46a63c6d1656
rezadrian01 <ahmadadrian324@gmail.com>
2024-10-06 13:09:53 +0700

    feat: finishing CRUD serviceList

--- controllers/serviceListController.js
+++ controllers/serviceListController.js
@@ -1,4 +1,4 @@
-const { responseHelper } = require('../helpers/responseHelper');
+const { responseHelper, errorHelper } = require('../helpers/responseHelper');
 const ServiceList = require('../models/ServiceList');
 
 const getServiceList = async (req, res, next) => {
@@ -11,5 +11,50 @@ const getServiceList = async (req, res, next) => {
     }
 }
 
+const createServiceList = async (req, res, next) => {
+    try {
+        const { serviceName } = req.body;
+        const validName = serviceName?.trim();
+        if (!validName) errorHelper("Invalid service name", 422)
+        const newService = new ServiceList({
+            name: validName
+        })
+        await newService.save();
+        responseHelper(res, "Success add new service", 201, true, { ...newService._doc })
+    } catch (err) {
+        if (!err.statusCode) err.statusCode = 500;
+        next(err);
+    }
+}
 
-module.exports = { getServiceList }
+const updateServiceName = async (req, res, next) => {
+    try {
+        const { serviceId, updatedName } = req.body;
+        const existingService = await ServiceList.findById(serviceId);
+        if (!existingService) errorHelper("Service not found", 404);
+
+        const validName = updatedName?.trim();
+        existingService.name = validName;
+        const updatedService = await existingService.save();
+        responseHelper(res, "Success update service name", 200, true, { ...updatedService._doc })
+    } catch (err) {
+        if (!err.statusCode) err.statusCode = 500;
+        next(err)
+    }
+}
+
+const deleteServiceName = async (req, res, next) => {
+    try {
+        const { serviceId } = req.body;
+        const existingService = await ServiceList.findById(serviceId);
+        if (!existingService) errorHelper("Service not found", 404);
+        await ServiceList.findByIdAndDelete(serviceId);
+        responseHelper(res, "Success delete service", 200, true)
+    } catch (err) {
+        if (!err.statusCode) err.statusCode = 500;
+        next(err);
+    }
+}
+
+
+module.exports = { createServiceList, getServiceList, updateServiceName, deleteServiceName }
--- routes/serviceListRoute.js
+++ routes/serviceListRoute.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { getServiceList } = require('../controllers/serviceListController');
+const { getServiceList, createServiceList, updateServiceName, deleteServiceName } = require('../controllers/serviceListController');
 const router = express.Router();
 
-router.get('/', getServiceList)
+router.get('/', getServiceList);
+router.post('/', createServiceList);
+router.put('/', updateServiceName);
+router.delete('/', deleteServiceName);
 
 module.exports = router
